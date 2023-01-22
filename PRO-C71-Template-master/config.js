0871dcbe93df0fa4177ecf46fdd31fbfb268886f
1charl <105971215+1charl@users.noreply.github.com>
2023-01-22 09:54:34 +0000

    Add files via upload

---
+++ PRO-C71-Template-master/config.js
@@ -0,0 +1,16 @@
+import firebase from "firebase";
+require("@firebase/firestore");
+
+//Paste your firebaseConfig here
+const firebaseConfig = {
+    apiKey: "AIzaSyCGJkILU5w0ALp9J4M_59RNMXhzCzve2vQ",
+    authDomain: "project-71-9526f.firebaseapp.com",
+    projectId: "project-71-9526f",
+    storageBucket: "project-71-9526f.appspot.com",
+    messagingSenderId: "328778373822",
+    appId: "1:328778373822:web:5222c466e92e297b815cef"
+  };
+//
+firebase.initializeApp(firebaseConfig);
+
+export default firebase.firestore();
